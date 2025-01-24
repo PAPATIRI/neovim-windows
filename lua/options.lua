473e42f2d375a1ada74ac5196256ab965e4f685e
PAPATIRI <syariftaufik11@gmail.com>
2025-01-24 21:13:51 +0700

    fix colorscheme and update packages

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.titlestring = "I'm using neovim btw"
+
 -- setting for chad nvimterm
 vim.o.shell = "powershell.exe"
 vim.o.shellxquote = ""
@@ -11,41 +13,41 @@ vim.o.shellpipe = "| Out-File -Encoding UTF8 %s"
 vim.o.shellredir = "| Out-File -Encoding UTF8 %s"
 
 local options = {
-	termguicolors = true,
-	number = true,
-	relativenumber = true,
-	shiftwidth = 4,
-	tabstop = 4,
-	softtabstop = 4,
-	expandtab = true, --use space to make tabs
-	swapfile = false, -- no swap file
-	backup = false, -- no backup file
-	mouse = "a", -- allow mouse
-	ignorecase = true, -- ignore sensitive case for searching
-	smartcase = true, -- ignore uppercase unless the search term has an uppercase
-	hlsearch = false, -- highlight the result of the search term
-	breakindent = true, -- preserve the indentation of a virtual line
-	cursorline = true,
-	clipboard = "unnamedplus", -- use the clipboard register for all operations except yank
-	autoindent = true, -- copy indent from current line when starting a new line
-	encoding = "utf-8", -- set the char encoding used inside neovim
-	fileencoding = "utf-8", -- set the char encoding for the file of the buffer
-	title = true, -- show the title on window with the value of titlestring
-	wildmenu = true, -- command line completion operates in an enhanced mode
-	showcmd = true, -- show partial command in the last line of the screen
-	cmdheight = 1, -- more space in the neovim command line for displaying message
-	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-	conceallevel = 0, -- so '' show in markdown file
-	pumheight = 10, -- pop up menu height
-	showmode = false, -- hide status like --INSERT--
-	undofile = true, -- enable persistent undo
-	signcolumn = "yes", -- always show the sign column , otherwise it would shift the text each time
-	scrolloff = 4,
-	updatetime = 50,
+    termguicolors = true,
+    number = true,
+    relativenumber = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    softtabstop = 4,
+    expandtab = true,                        --use space to make tabs
+    swapfile = false,                        -- no swap file
+    backup = false,                          -- no backup file
+    mouse = "a",                             -- allow mouse
+    ignorecase = true,                       -- ignore sensitive case for searching
+    smartcase = true,                        -- ignore uppercase unless the search term has an uppercase
+    hlsearch = false,                        -- highlight the result of the search term
+    breakindent = true,                      -- preserve the indentation of a virtual line
+    cursorline = true,
+    clipboard = "unnamedplus",               -- use the clipboard register for all operations except yank
+    autoindent = true,                       -- copy indent from current line when starting a new line
+    encoding = "utf-8",                      -- set the char encoding used inside neovim
+    fileencoding = "utf-8",                  -- set the char encoding for the file of the buffer
+    title = true,                            -- show the title on window with the value of titlestring
+    wildmenu = true,                         -- command line completion operates in an enhanced mode
+    showcmd = true,                          -- show partial command in the last line of the screen
+    cmdheight = 1,                           -- more space in the neovim command line for displaying message
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    conceallevel = 0,                        -- so '' show in markdown file
+    pumheight = 10,                          -- pop up menu height
+    showmode = false,                        -- hide status like --INSERT--
+    undofile = true,                         -- enable persistent undo
+    signcolumn = "yes",                      -- always show the sign column , otherwise it would shift the text each time
+    scrolloff = 4,
+    updatetime = 50,
 }
 
 vim.opt.shortmess:append("c")
 
 for key, value in pairs(options) do
-	vim.opt[key] = value
+    vim.opt[key] = value
 end
